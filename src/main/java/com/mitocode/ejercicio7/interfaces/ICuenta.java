52ae488e59aac5ed473570d4fcd8d4fa4927890e
javierefe <jcfloresq@gmail.com>
2020-02-17 20:38:57 -0500

    clase 3

---
+++ src/main/java/com/mitocode/ejercicio7/interfaces/ICuenta.java
@@ -0,0 +1,8 @@
+package com.mitocode.ejercicio7.interfaces;
+
+public interface ICuenta {
+
+	void retiro(double monto);
+	//por defecto toda interaz sus metodos son publicos y sus metodos abstractos
+	void deposito(double monto);
+}
